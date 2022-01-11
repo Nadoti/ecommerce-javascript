476b6c96e23e32ea7df5ddde46cd8ca8373b2ce8
Douglas <dodo_nadoti@hotmail.com>
2022-01-11 16:36:13 -0300

    Primeiro Commit

---
+++ back/data.js
@@ -0,0 +1,53 @@
+export default  {
+  products = [
+    {
+      id: 1,
+      name: morango,
+      qntStock: 16,
+      image: '/images/morango.png'
+    },
+    {
+      id: 2,
+      name: Pera,
+      qntStock: 20,
+      image: '/images/pera.png'
+    },
+    {
+      id: 3,
+      name: Cebola,
+      qntStock: 12,
+      image: '/images/cebola.png'
+    },
+    {
+      id: 4,
+      name: Tomate,
+      qntStock: 8,
+      image: '/images/tomate.png'
+    },
+    {
+      id: 5,
+      name: Limão,
+      qntStock: 36,
+      image: '/images/limao.png'
+    },
+    {
+      id: 6,
+      name: Berinjela,
+      qntStock: 4,
+      image: '/images/berinjela.png'
+    },
+    {
+      id: 7,
+      name: Brocolis,
+      qntStock: 9,
+      image: '/images/brocolis.png'
+    },
+    {
+      id: 8,
+      name: Batata,
+      qntStock: 22,
+      image: '/images/batata.png'
+    },
+
+  ]
+}
